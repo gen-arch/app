2e26ef31dbd6d22cfa9e63c061485c00cd07ff20
gen <ymdtshm@gmail.com>
2019-05-29 18:38:36 +0900

    update

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,8 +1,20 @@
 require_relative 'base'
+require "sinatra/json"
+require "sinatra/namespace"
 
 class Api < Base
   before do
     content_type :json
     protected! if production?
+  end
+  # api version 1
+  namespace '/v1' do
+    get '/get' do
+      json { respons: 'hello world'}
+    end
+  end
+
+  # api version 1
+  namespace '/v2' do
   end
 end
